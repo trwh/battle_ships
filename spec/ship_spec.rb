41c477d8ba3fdb81734a90e2032f45093403e26a
Tim Harrison <mail@trwh.co.uk>
2015-09-07 16:50:48 +0100

    Tests for Ship.fire

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,6 +5,8 @@ require_relative '../lib/ship.rb'
 describe Ship do
 
   let (:ship){ Ship.new(3) }
+  let (:origin){ [0,0]}
+  let (:orientation){ 'N'}
 
   it 'ship makes ship object' do
       expect(Ship).to respond_to(:new).with(1).argument
@@ -15,8 +17,6 @@ describe Ship do
   end
 
   it 'when placed, ship has location and orientation' do
-    origin = [0,0]
-    orientation = 'N'
     expect(ship.place(origin, orientation)).to eq([[0,0],[0,1],[0,2]])
   end
 
@@ -24,7 +24,12 @@ describe Ship do
     expect(ship).to respond_to(:fire).with(1).argument
   end
 
-  xit 'ship reports hit'
+  it 'ship reports hit' do
+    ship.place(origin, orientation)
+    expect(ship.fire([0,1]) ).to eq(true)
+    expect(ship.fire([0,3]) ).to eq(false)
+  end
+
 
   xit 'ship reports sunk'
 
