c24f81a8ada0ed9acd0da9e4f7d155f3849d3214
Emily Worrall <worrall.emily@gmail.com>
2015-09-08 16:21:57 +0100

    Added tests for hits and misses

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,18 @@
 class Player
 
+  attr_accessor :turn
+
+  def initialize(player_no)
+    @player_no = player_no
+    if @player_no == 1
+      @turn = true
+    else
+      @turn = false
+    end
+
+    @hits   = []
+    @misses = []
+  end
+
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,16 +4,25 @@ describe Player do
 
 let (:player1){ Player.new(1) }
 let (:player2){ Player.new(2) }
+let (:board_hit){ double(:board, fire: true) }
+let (:board_miss){ double(:board, fire: false) }
+
 
 it 'takes a single argument' do
       expect(Player).to respond_to(:new).with(1).argument
   end
 
   it 'knows whather it\'s this player\'s turn' do
-    expect(player1).to be_turn
-    expect(player2).not_to be_turn
+    expect(player1.turn).to eq true
+    expect(player2.turn).to eq false
   end
 
-  xit 'knows where the player has fired on the opponent\'s board'
+  it 'knows where the player has fired on the opponent\'s board' do
+    player1.fire([0,0])
+    player1.fire([0,1])
+    player1.fire([0,2])
+    expect(player1.hits).to eq [[0,0], [0,1]]
+    expect(player1.misses).to eq [[0,2]]
+  end
 
 end
