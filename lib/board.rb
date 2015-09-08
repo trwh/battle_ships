28e7b49c4a53436211f51b6f41f6d0117073182f
Emily Worrall <worrall.emily@gmail.com>
2015-09-08 18:02:24 +0100

    Shizzle

--- lib/board.rb
+++ lib/board.rb
@@ -7,20 +7,6 @@ class Board
     @ships = ships
   end
 
-  def in_bounds?(ship)
-    ship.cells.each do |coordinates|
-      coordinates.each do |i|
-        return false if i > (size - 1)
-      end
-    end
-
-      true
-  end
-
-  def overlapped?(ship1, ship2)
-    !( (ship1.cells & ship2.cells).empty? )
-  end
-
   def result?(*ships)
     ships.each do |i|
       return false if !(i.sunk?)
@@ -29,8 +15,39 @@ class Board
     true
   end
 
+  def place(ship, coordinates, orientation)
+    ship.place(coordinates, orientation)
+
+    fail 'Ship is out-of-bounds.' unless in_bounds?(ship)
+
+    @ships.each do |i|
+      fail 'Ship overlaps an existing ship.' if overlapped?(i, ship)
+    end
+
+    @ships << ship
+
+    return true
+  end
+
   def fire(coordinates)
 
   end
 
+  private
+
+  def in_bounds?(ship)
+  ship.cells.each do |coordinates|
+    coordinates.each do |i|
+      return false if i > (size - 1)
+    end
+  end
+
+    true
+  end
+
+  def overlapped?(ship1, ship2)
+    !( (ship1.cells & ship2.cells).empty? )
+  end
+
 end
+
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,11 +3,12 @@ require_relative '../lib/board.rb'
 describe Board do
 
   let (:board10){ Board.new(10, []) }
-  let (:ship1){ double(:ship, cells: [[0,0], [0,1], [0,2]]) }
-  let (:ship2){ double(:ship, cells: [[0,10], [1,10], [2,10]]) }
-  let (:ship3){ double(:ship, cells: [[0,2], [0,3], [0,4]]) }
+  let (:ship1){ double(:ship, place: nil, cells: [[0,0], [0,1], [0,2]]) }
+  let (:ship2){ double(:ship, place: nil, cells: [[0,10], [1,10], [2,10]]) }
+  let (:ship3){ double(:ship, place: nil, cells: [[0,2], [0,3], [0,4]]) }
   let (:floating_ship){ double(:ship, sunk?: false) }
   let (:sunk_ship){ double(:ship, sunk?: true) }
+  let (:basic_ship){ double(:ship) }
 
   it 'takes two arguments' do
     expect(Board).to respond_to(:new).with(2).argument
@@ -17,23 +18,23 @@ describe Board do
     expect(board10.size).to eq(10)
   end
 
-  it 'receives in_bounds? with a single argument' do
-    expect(board10).to respond_to(:in_bounds?).with(1).argument
-  end
+  # it 'receives in_bounds? with a single argument' do
+  #   expect(board10).to respond_to(:in_bounds?).with(1).argument
+  # end
 
-  it 'tests whether ship is within bounds' do
-    expect(board10).to be_in_bounds(ship1)
-    expect(board10).not_to be_in_bounds(ship2)
-  end
+  # it 'tests whether ship is within bounds' do
+  #   expect(board10).to be_in_bounds(ship1)
+  #   expect(board10).not_to be_in_bounds(ship2)
+  # end
 
-  it 'receives overlapped? with two arguments' do
-    expect(board10).to respond_to(:overlapped?).with(2).argument
-  end
+  # it 'receives overlapped? with two arguments' do
+  #   expect(board10).to respond_to(:overlapped?).with(2).argument
+  # end
 
-  it 'tests whether ships are overlapped' do
-    expect(board10).not_to be_overlapped(ship1, ship2)
-    expect(board10).to be_overlapped(ship1, ship3)
-  end
+  # it 'tests whether ships are overlapped' do
+  #   expect(board10).not_to be_overlapped(ship1, ship2)
+  #   expect(board10).to be_overlapped(ship1, ship3)
+  # end
 
   it 'receives result? with (at least) one argument' do
     expect(board10).to respond_to(:result?).with(1).argument
@@ -44,9 +45,14 @@ describe Board do
     expect(board10).to be_result(sunk_ship, sunk_ship, sunk_ship)
   end
 
-  it 'accepts the placement of a ship' do
-    # in here, we accept a ship object into the @ships array, testing it using overlapped? and in_bounds? The individual ships need to be initialised themselves with Ship.place. The user passes a ship object to place, with coordinates. The board checks and then places the ship using its own method.
+  it 'receives place with three arguments' do
+    expect(board10).to respond_to(:place).with(3).argument
+  end
 
+  it 'accepts the placement of a ship' do
+    expect(board10.place(ship1, [0,0], 'N')).to eq true
+    expect{ board10.place(ship2, [0,10], 'E') }.to raise_error 'Ship is out-of-bounds.'
+    expect{ board10.place(ship3, [0,2], 'N') }.to raise_error 'Ship overlaps an existing ship.'
   end
 
   it 'receives fire with one argument' do
